a2ca3e431265fd9fef417c478e70431d7f186560
dmateusb <dmateusb@unal.edu.co>
2022-06-21 21:27:47 -0500

    Añadir enpoint para traducir

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,15 @@
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class Main {
     public static void main(String[] args) {
         Server.run();
-        try{
-            HTMLLexer lexer;
-            if (args.length>0)
-                lexer = new HTMLLexer(CharStreams.fromFileName(args[0]));
-            else
-                lexer = new HTMLLexer(CharStreams.fromStream(System.in));
-
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            HTMLParser parser = new HTMLParser(tokens);
-
-            ParseTree tree = parser.htmlDocument();
-            ParseTreeWalker walker = new ParseTreeWalker();
-            walker.walk(new Traductor(), tree);
-            System.out.println(); // print a \n after translation
-
-
-        } catch (Exception e){
-            System.err.println("Error (Main)" + e);
-        }
     }
+
 }
--- src/webserver/TranslateController.java
+++ src/webserver/TranslateController.java
@@ -1,15 +1,42 @@
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class TranslateController{
 
     public static String post(Request req, Response res){
-        String body = req.body();
-        JSONObject body_json = new JSONObject(body);
-        String original_text = (String) body_json.get("text");
 
-        return "";
+        // Capture body param
+        String original_text = req.body();
+        //JSONObject body_json = new JSONObject(body);
+        //String original_text = (String) body_json.get("text");
 
+        // New Console Stream
+
+        PrintStream previousConsole = System.out;
+        ByteArrayOutputStream newConsole = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(newConsole));
+
+        // Create lexer from body param
+
+        HTMLLexer lexer = new HTMLLexer(CharStreams.fromString(original_text));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        HTMLParser parser = new HTMLParser(tokens);
+
+        ParseTree tree = parser.htmlDocument();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(new Traductor(), tree);
+        previousConsole.println(newConsole.toString());
+        System.setOut(previousConsole);
+        String translated_text =  newConsole.toString();
+        res.status(200);
+        return translated_text;
     }
 }
